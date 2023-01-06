c7b50351f4cf7fd24568f85c9d3526619999854d
svensve <113555050+svensve@users.noreply.github.com>
2023-01-06 14:31:31 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,32 @@
+
+# Create an EC2 instance
+resource "aws_instance" "web_server" {
+  ami           = "ami-01b8d743224353ffe" #Ubuntu Server, 22.04 LTS
+  instance_type = "t2.micro"
+  security_groups = ["allow_http"]
+
+
+  # Install Apache webserver and write "Hello, World"
+  user_data = <<EOF
+    #!/bin/bash
+    apt-get update
+    apt-get install -y apache2
+    echo 'Hello, World' > /var/www/html/index.html
+  EOF
+}
+
+# Expose port 8080
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP traffic Task3"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "url" {
+  value = "http://${aws_instance.web_server.public_ip}:8080"
+}
